e136f8615e5b9412c2eef20dc1499118f151e6c8
Quan Le Van <quanbka.cntt@gmail.com>
2022-05-28 10:16:23 +0700

    ok

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+// die;
+$actual_link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? "https" : "http") . "://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}";
+$origin_link = str_replace( 'https://mercedes-benz.test/', 'https://mercedes-benz.hssvn.edu.vn/', "$actual_link");
+$origin_link = preg_replace("/\?(.*)/", "", $origin_link);
+// print_r($actual_link);
+// print_r($origin_link);
+// print_r(( $_SERVER['REQUEST_URI'] ));
+$array = (explode("/", $_SERVER['REQUEST_URI']));
+unset($array[count($array) - 1]);
+$path = join("/", $array);
+$path = ltrim( $path, "/");
+echo ("mkdir -p '$path'");
+echo exec("mkdir -p '$path'");
+chdir($path);
+echo getcwd() . "\n";
+echo exec("wget '$origin_link'");
+die;
